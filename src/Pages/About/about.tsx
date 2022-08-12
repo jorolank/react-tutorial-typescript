2f8203a074c5c5308483c5f760df952f17d7cd16
kjorolan <kimberly@ajio.co.jp>
2022-08-12 11:05:58 +0800

    link

--- src/Pages/About/about.tsx
+++ src/Pages/About/about.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "react-router-dom";
 
 const About = () => {
     return (
@@ -14,6 +15,8 @@ const About = () => {
                 their
                 marks in a horizontal, vertical, or diagonal row is the winner.
             </p>
+            <p>Ref: <Link to="https://reactjs.org/tutorial/tutorial.html#function-components">
+                https://reactjs.org/tutorial/tutorial.html#function-components</Link></p>
         </div>
     )
 }
